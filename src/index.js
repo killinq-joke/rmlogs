d74ecd6b682cbad8a00e3bab07f29f45fc21a668
zakaria touzri <zakadmaster@gmail.com>
2020-06-23 20:48:51 +0200

    --wip-- [skip ci]

--- src/index.js
+++ src/index.js
@@ -4,6 +4,14 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+function noop() {}
+
+if (process.env.NODE_ENV !== 'development') {
+  console.log = noop;
+  console.warn = noop;
+  console.error = noop;
+}
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
